bafe15e9eb34d3fa3a72186b4399a064ade78a19
Starz-program <vanessa.contreras@ucas-edu.net>
2026-09-09 14:52:01 -0600

    Variables Notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# VC, Variables Notes <= for people to read, not the computer
+
+name = input ("Tell me your name: ")
+
+print ("CSP is the best class ever!!!!!!")
+print ("pink classroom is the best!")
+print ("hello", name, "welcome to class!")
