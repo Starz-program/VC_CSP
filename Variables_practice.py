71166007fd9bc1325bf13dadb2fe507005257d47
Starz-program <vanessa.contreras@ucas-edu.net>
2026-09-10 14:32:33 -0600

    Variable Practices

---
+++ Variables_practice.py
@@ -0,0 +1,13 @@
+#VC, Variables Practice
+
+name = "Linda"
+number = "4"
+big = "248"
+breakfast = "cereal"
+color = "fuscia"
+school = "UCAS"
+year = "2026"
+eye = "brown"
+age = "18"
+subject = "Biology"
+print (name, number, big, breakfast, color, school, year, eye, age, subject)
