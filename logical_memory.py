abe47ce6d4f4757209953d4a81d5d332e8b14f5b
Starz-program <vanessa.contreras@ucas-edu.net>
2026-09-15 14:30:08 -0600

    logical_memory notes

---
+++ logical_memory.py
@@ -0,0 +1,37 @@
+#VC, Logical and physical memory notes
+
+name = "Alex"
+
+Age = 14
+
+score = 10
+
+Score = 25
+
+height = 5.9
+
+city = "Denver"
+
+zip_code = 80202
+
+price1 = 10
+
+price2 = 10
+
+print(f"The variable name has the value of {name} and is saved at {id(name)}")
+
+print(f"The variable Age has the value of {Age} and is saved at {id(Age)}")
+
+print(f"The variable score has the value of {score} and is saved at {id(score)}")
+
+print(f" The variable Score has the value of {Score} and is saved at {id(Score)}")
+
+print(f"The variable height has the value of {height} and is saved at {id(height)}")
+
+print(f"The variable city has the value of {city} and is saved at {id(city)}")
+
+print(f"The variable zip_code has the value of {zip_code} and is saved at {id(city)}")
+
+print(f"The variable price1 has the value of {price1} and is saved at {id(price1)}")
+
+print(f"The variable price2 has the value of {price2} and is saved at {id(price2)}")
